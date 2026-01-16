277f1d93d1cbd088579af3dbadf108f64bfd1314
Long <sy10511@syss.edu.hk>
2026-01-16 23:36:18 +0800

    improve static_array init logic

--- static_array.cpp
+++ static_array.cpp
@@ -1,14 +1,9 @@
 template <typename T, std::size_t N>
 struct static_array : public std::array<T, N> {
     constexpr static_array() = default;
-    constexpr explicit static_array(const T& val) {
-        this->fill(val);
-    }
-    constexpr static_array(std::initializer_list<T> il) {
-        std::size_t i = 0;
-        for (auto it = il.begin(); it != il.end() && i < N; ++it, ++i) (*this)[i] = *it;
-        for (; i < N; ++i) (*this)[i] = T{};
-    }
+    constexpr explicit static_array(const T& val) { this->fill(val); }
+    template <typename... U> requires (sizeof...(U) == N && (std::convertible_to<U, T> && ...))
+    constexpr static_array(U&&... u) : std::array<T, N>{ { static_cast<T>(std::forward<U>(u))... } } {}
     constexpr void clear() noexcept {
         if constexpr (std::is_trivially_default_constructible_v<T> &&
                       std::is_trivially_copyable_v<T> &&
@@ -18,15 +13,15 @@ struct static_array : public std::array<T, N> {
             this->fill(T{});
         }
     }
-    friend std::istream& operator>>(std::istream& in, static_array& a) {
-        for (auto& x : a) in >> x;
-        return in;
-    }
     friend std::ostream& operator<<(std::ostream& out, const static_array& a) {
         for (std::size_t i = 0; i < N; ++i) {
             out << a[i];
             if (i + 1 < N) out << ' ';
         }
         return out;
+    }
+    friend std::istream& operator>>(std::istream& in, static_array& a) {
+        for (auto& x : a) in >> x;
+        return in;
     }
 };
